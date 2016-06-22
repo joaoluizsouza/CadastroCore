e1b3ec7e8fc862a5c6affc75d6fa06b600950a69
Joao <joaoluizgs@gmail.com>
2016-06-22 13:58:47 -0300

    Service Product

--- AngularApp/app/product/product.component.ts
+++ AngularApp/app/product/product.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from './product.service';
+import { Product } from './product';
 
 @Component({
     moduleId: module.id,
-    templateUrl: 'product.component.html'
+    templateUrl: 'product.component.html',
+    providers:[ProductService]
 })
 export class ProductComponent implements OnInit {
-    constructor() { }
+    public products: Product[] = [];
+    constructor(private _productService:ProductService) { }
 
-    ngOnInit() { }
+    ngOnInit() { 
+        this._productService.getProducts()
+            .subscribe(data => this.products = data, error => console.log(error));
+    }
 
+    onClick(){
+        console.log(this.products);
+    }
 }
---
+++ AngularApp/app/product/product.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from "@angular/http";
+import { Product } from "./product";
+import { Observable } from "rxjs/Observable";
+import "rxjs/Rx";
+
+@Injectable()
+export class ProductService {
+    private headers: Headers;
+    private _apiUrl: string = "/app/mocks/";
+    constructor(private _http: Http) {
+        this.headers = new Headers({ 'Content-Type': 'application/json' });
+    }
+
+    getProducts() {
+        return this._http.get(this._apiUrl + "products.js")
+            .map(res => res.json())
+            .catch(this.throwError);
+    }
+    
+    private throwError(response) {
+        return Observable.throw(response.json().error || "Server error")
+    }
+
+}

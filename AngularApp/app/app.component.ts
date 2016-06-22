46fa5bbad42ad1482e6ad9132724dc4b1b73778f
Joao <joaoluizgs@gmail.com>
2016-06-22 09:07:33 -0300

    Navigation

--- AngularApp/app/app.component.ts
+++ AngularApp/app/app.component.ts
@@ -1,6 +1,21 @@
 import { Component } from '@angular/core';
+import { HTTP_PROVIDERS } from '@angular/http';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductComponent } from './product/product.component';
+
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  templateUrl: 'app.component.html',
+  directives: [ROUTER_DIRECTIVES],
+  providers: [
+    HTTP_PROVIDERS,
+    ROUTER_PROVIDERS
+  ]
 })
-export class AppComponent { }
+@RouteConfig([
+  { path: '/dashboard', name: 'Dashboard', component: DashboardComponent, useAsDefault: true },
+  { path: '/product', name: 'Product', component: ProductComponent}
+])
+export class AppComponent {}

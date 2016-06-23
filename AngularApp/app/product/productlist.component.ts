aeb4973cfdfa8fe319389787a84603edc7b8ddf7
Joao <joaoluizgs@gmail.com>
2016-06-23 08:51:25 -0300

    @Input

--- AngularApp/app/product/productlist.component.ts
+++ AngularApp/app/product/productlist.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Product } from './Product';
 
 @Component({
     moduleId: module.id,
     selector: 'product-list',
-    templateUrl: 'productlist.component.html',
-    inputs: ['products'],
+    templateUrl: 'productlist.component.html'
 })
 export class ProductListComponent implements OnInit {
+    @Input()
+    private products: Product[];
+
     constructor() { }
-
-    ngOnInit() { 
-    }
-
+    ngOnInit() { }
 }

848924d508a10507e8aa67d92320ec6b5205109c
Joao <joaoluizgs@gmail.com>
2016-06-24 17:27:28 -0300

    Forms

--- AngularApp/app/main.ts
+++ AngularApp/app/main.ts
@@ -2,5 +2,6 @@ import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app.component';
 import { ROUTER_PROVIDERS } from "@angular/router-deprecated";
 import { HTTP_PROVIDERS } from '@angular/http';
+import { disableDeprecatedForms, provideForms } from '@angular/forms';
 
-bootstrap(AppComponent,[ROUTER_PROVIDERS, HTTP_PROVIDERS]);
+bootstrap(AppComponent,[ROUTER_PROVIDERS, HTTP_PROVIDERS, disableDeprecatedForms(),provideForms()]);
--- AngularApp/app/product/product-detail.component.ts
+++ AngularApp/app/product/product-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/common';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router,RouteParams } from "@angular/router-deprecated";
 import { ProductService } from './product.service';
 import { Product } from './Product';
@@ -9,14 +10,22 @@ import { Product } from './Product';
     providers:[ProductService]
 })
 export class ProductDetailComponent implements OnInit {
-    public _product: Product;
+    @Input('product') public _product: Product;
+
+    active = true;
+    public model : Product;
+
+        
     constructor(private _routeParams: RouteParams, private _productService: ProductService, private _router: Router) { }
 
     ngOnInit() {
+        this.model = {name:"Joao",id:10,image:"",price:20};
         let idProduct = <number><any>this._routeParams.get("id");
         //this._productService.getProduct(idProduct).subscribe(data => this._product = data, error => console.log(error));
      }
     goList(): void {
         this._router.navigate(["Product"]);
     }
+
+    get diagnostic() { return JSON.stringify(this.model); }
 }

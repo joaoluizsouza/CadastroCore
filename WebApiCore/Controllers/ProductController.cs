d0ca3985462ffeff965fbfc778758c8c3c8620a1
João Luiz Souza <joaoluizgs@gmail.com>
2016-07-13 20:31:33 -0300

    API FULL

--- WebApiCore/Controllers/ProductController.cs
+++ WebApiCore/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApiCore.Infra;
 using WebApiCore.Model;
 
@@ -34,20 +35,27 @@ namespace WebApiCore.Controllers
         [HttpPost]
         public void Post([FromBody] Product product)
         {
+            product.Id = _dbContext.Products.Count() + 1;
             _dbContext.Products.Add(product);
             _dbContext.SaveChanges();
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] Product product)
         {
+            product.Id = id;
+            _dbContext.Entry(product).State = EntityState.Modified;
+            _dbContext.SaveChanges();
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var prod = _dbContext.Products.First(p => p.Id == id);
+            _dbContext.Entry(prod).State = EntityState.Deleted;
+            _dbContext.SaveChanges();
         }
     }
 }
